8f4367d2b4ebcecc2b3f99d374e3a8324fab8e7c
rnnnikita <rudenko.nikita93@gmail.com>
2021-10-09 15:47:39 +0300

    updated bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,5 +3,6 @@
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 cd "${SCRIPT_DIR}" || exit
 
-python -m venv .venv --upgrade-deps
+python -m venv .venv
+python.exe -m pip install --upgrade pip
 pip install -r requirements.txt
