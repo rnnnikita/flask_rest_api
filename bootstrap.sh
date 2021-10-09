fc04cf583d7ab2f596f9b940d2307bf3b5786abc
ruden <asd@mail.ru>
2021-10-09 16:26:42 +0300

    updated readme to reflect setup changes

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,5 +4,6 @@ SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd "${SCRIPT_DIR}" || exit
 
 python -m venv .venv
+source .venv/Scripts/activate
 python.exe -m pip install --upgrade pip
 pip install -r requirements.txt
